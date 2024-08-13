d9ea320195c92b458076cb74f6c28edf4961b11f
Abdurrahman Furkan ADAR <frkndr01@gmail.com>
2024-08-14 00:43:22 +0300

    personel-card-final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import linkedin from "./linedln.svg";
+import email from "./email.svg";
+import twitter from "./twitter.svg";
+import instagram from "./instagram.svg";
+import github from "./github.svg";
+import facebook from "./facebook.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <nav className="navbar">
+        <img className="pp-photo" src="furkan.png" />
+        <h className="name">Furkan Adar</h>
+        <h className="title">Frontend Developer</h>
+      </nav>
+      <div className="button">
+        <button onClick={() => window.location = 'mailto:frkndr01@gmail.com'} className="btn-email">
+        <img className="icon" src={email} width="15px" />
+        Email
+      </button>
+      <a href="https://www.linkedin.com/in/abdurrahman-furkan-adar-708b97235/"><button className="btn-linkedln"><img className="icon" src={linkedin} width="15px" />
+        Linkedln
+      </button></a>
+      </div>
+      <div className="main">
+        <h3>About</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am a frontend developer with a particular interest in making things
+          simple and automating daily tasks. I try to keep up with security and
+          best practices, and am always looking for new things to learn.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Interests</h3>
+        <p>
+          Food expert. Music scholar. Reader. Internet fanatic.
+          Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+        </p>
+      </div>
+      <footer className="footer">
+      <a href="https://x.com/"><img className="footer-icon" src={twitter}/></a>
+      <a href="https://www.facebook.com/"><img className="footer-icon" src={facebook}/></a>
+      <a href="https://www.instagram.com/"><img className="footer-icon" src={instagram}/></a>
+      <a href="https://github.com/furkandr01"><img className="footer-icon" src={github}/></a>
+      </footer>
     </div>
   );
 }
